f6785f560af2e289bd89feead3684e89fcdfd605
vanshg101 <vanshgularia@gmail.com>
2024-08-25 11:06:19 +0530

    t

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -158,7 +158,7 @@ const logoutUser = asyncHandler(async(req,res)=>{
 const refreshAccessToken = asyncHandler(async(req,res)=>{
    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-   if (incomingRefreshToken) {
+   if (!incomingRefreshToken) {
      throw new APIError(401,"unauthorized request")
    }
 
