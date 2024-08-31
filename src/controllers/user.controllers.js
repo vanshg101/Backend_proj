6ab8201b7625959859a06907b958bb9dfe00ec6a
vanshg101 <vanshgularia@gmail.com>
2024-08-31 23:28:47 +0530

    update error

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -138,8 +138,8 @@ const logoutUser = asyncHandler(async (req, res) => {
   User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: {
-        refreshToken: undefined,
+      $unset: {
+        refreshToken: 1,
       },
     },
     {
